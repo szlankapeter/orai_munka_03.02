b06fee9e8e60b9a7c9bc50e8d34be072eacd784e
CsoltiP <csolti@szamalk-szalezi.hu>
2023-03-01 15:26:49 +0100

    dobasok es statisztika ures metodusok

--- SzamolosFeladatok/src/tombbel/DobasKettoKockaval.java
+++ SzamolosFeladatok/src/tombbel/DobasKettoKockaval.java
@@ -3,9 +3,21 @@ package tombbel;
 public class DobasKettoKockaval {
 
     public static void main(String[] args) {
-        System.out.println(" 5 ****** (2 db)");
-        System.out.println("11 *** (1 db)");
-        System.out.println("12 ****** (2 db)");
+        /* 1 lehetséges kimenet 5 dobá esetén */
+//        System.out.println(" 5 ****** (2 db)");
+//        System.out.println("11 *** (1 db)");
+//        System.out.println("12 ****** (2 db)");
+        
+        int[] dobasok = kettoKockavalDob(5);
+        statisztika(dobasok);
+    }
+
+    private static int[] kettoKockavalDob(int db) {
+        return new int[1];
+    }
+
+    private static void statisztika(int[] adatok) {
+        
     }
     
 }
