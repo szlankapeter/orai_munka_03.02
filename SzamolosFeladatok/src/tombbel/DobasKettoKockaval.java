8507701c3f2589a1244409322a75936795dd023e
CsoltiP <csolti@szamalk-szalezi.hu>
2023-03-01 15:51:05 +0100

    kettoKockavalDob(int) kesz

--- SzamolosFeladatok/src/tombbel/DobasKettoKockaval.java
+++ SzamolosFeladatok/src/tombbel/DobasKettoKockaval.java
@@ -1,7 +1,11 @@
 package tombbel;
 
+import java.util.Random;
+
 public class DobasKettoKockaval {
 
+    private static Random rnd = new Random();
+    
     public static void main(String[] args) {
         /* 1 lehetséges kimenet 5 dobá esetén */
 //        System.out.println(" 5 ****** (2 db)");
@@ -13,11 +17,17 @@ public class DobasKettoKockaval {
     }
 
     private static int[] kettoKockavalDob(int db) {
-        return new int[1];
+        int[] dobasok = new int[13];
+        for (int i = 0; i < db; i++) {
+            int dobas = rnd.nextInt(2,13);
+            dobasok[dobas]++; //dobasok[dobas] = dobasok[dobas] + 1;
+        }
+        return dobasok;
+
     }
 
     private static void statisztika(int[] adatok) {
-        
+        System.out.println(adatok);
     }
     
 }
