4df96b0461e813d83805b45c7e7ff42313933bd2
CsoltiP <csolti@szamalk-szalezi.hu>
2023-03-01 14:53:31 +0100

    kepernyoterv 1.

---
+++ SzamolosFeladatok/src/tombbel/DobasKettoKockaval.java
@@ -0,0 +1,11 @@
+package tombbel;
+
+public class DobasKettoKockaval {
+
+    public static void main(String[] args) {
+        System.out.println(" 5 ****** (2 db)");
+        System.out.println("11 *** (1 db)");
+        System.out.println("12 ****** (2 db)");
+    }
+    
+}
