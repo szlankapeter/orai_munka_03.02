1d788a69cac690be6d7a7236a26ca17faafc3b08
szlankapeter <szlankapeter@gmail.com>
2023-03-02 13:18:54 +0100

    elso feladat

--- SzamolosFeladatok/src/Ciklusok/Szamok.java
+++ SzamolosFeladatok/src/Ciklusok/Szamok.java
@@ -13,6 +13,17 @@ public class Szamok {
     
     public static void main(String[] args) {
         
+        for(int i = 2; i <= 4; i++){
+            for(int j = 2; j <= 4; j++){
+                for(int k = 2; i <= 4; k++){
+                    System.out.printf("%d%d%d\n", i, j, k);
+                    
+                }
+            }
+        }
+        
+        
+        
     }
     
     
