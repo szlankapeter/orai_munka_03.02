dbaaa29bab853b3139177f041cbc4d5c3a4455e1
szlankapeter <szlankapeter@gmail.com>
2023-03-02 13:22:43 +0100

    elso feladat

--- SzamolosFeladatok/src/Ciklusok/Szamok.java
+++ SzamolosFeladatok/src/Ciklusok/Szamok.java
@@ -4,6 +4,8 @@
  */
 package Ciklusok;
 
+import java.util.Scanner;
+
 /**
  *
  * @author szlan
@@ -12,10 +14,13 @@ public class Szamok {
     
     
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in, "latin2");
+        System.out.println("Szeretne számismétlést kérni?");
+        String valasz = sc.next();
         
         for(int i = 2; i <= 4; i++){
             for(int j = 2; j <= 4; j++){
-                for(int k = 2; i <= 4; k++){
+                for(int k = 2; k <= 4; k++){
                     System.out.printf("%d%d%d\n", i, j, k);
                     
                 }
